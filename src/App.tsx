cc130e0c4d23afb2bdb0b0a21cf29705f25d1b67
Luis Martin Mola Enriquez <molaenriquezl@gmail.com>
2025-09-18 19:05:24 -0400

    bb

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ const App: React.FC = () => {
 
   const longMessage = `
     Hoy es el día en que el mundo vio caer un ángel del cielo. Hoy el mundo se volvió perfecto, o mejor dicho, mi mundo.Ese día, sin saberlo, el universo me regaló lo más hermoso que tendría jamás, aunque claro eso no lo iba a saber hasta 16 años después ,cuando apareciste en mi vida para cambiarlo todo.
-
 Ya han pasado tres años y te miro sin dejar de preguntarme cómo un ser tan hermoso , tan puro ,tan especial ,tan perfecto me escogió a mí. Cada momento a tu lado se vuelve una película que no me importa si son de las que tienen un final feliz o triste simplemente quiero que no termine. Sentir tu cabello negro entre mis manos me hipnotiza… supongo que así se deben sentir las nubes. Esos instantes en tus hermosos ojitos chinos me atrapan y ya no puedo escapar, pero no importa, porque me quedaría la eternidad allí, sabiendo que solo me miran a mí.
 Cuando siento tu boquita carnosa acercarse, tu respiración cerca y luego me besas, mi mi piel se estremece, un escalofrío me recorre la nuca y el mundo entero desaparece. Luego me abrazas, reímos y solo quiero que el reloj se congele en ese presiso instante. Tus tatuajes, que tanto me enlcoquecen, encajan tan bien en ese perfecto rompecabezas que es tu cuerpo, el cual se ha convertido en mi mision: descifrarte, conoceter y hacerte sentir lo mismo que provocas en mí , cuando me tocas y haces que este mortal pise el cielo por un efimero instante.
 Pero si tu hermosura no bastara para adueñarte de mi corazón, eres también el ser más atento, detallista y cariñoso. Amas de la forma más pura que existe, me apoyas incondicionalmente en todo, eres inteligente, imaginativa, divertida… haces que el tiempo transcurra tan rápido que sé que, cuando lleguemos a viejitos en una granjita en SANAU, suspiraré resignado porque una vida no será suficiente para nuestro amor.
@@ -24,13 +23,26 @@ Feliz cumpleaños al ángel que se enamoró de un simple mortal. Te amoooo.
   };
 
   return (
-    <div className="app-container">
+    <div translate='no' className="app-container">
       {isStarted ? (
         <LoveCard message={longMessage.trim()} />
       ) : (
-        <button className="start-button" style={{margin:"20px"}} onClick={handleStart}>
-         ❤️❤️Para que nunca lo pierda , se rompa , ni lo olvides ,Te amo con todas mis cada parte de mi❤️
-        </button>
+        <div className="heart-start-button" onClick={handleStart}>
+          <svg viewBox="0 0 600 500">
+            <path
+              d="M300,150 C200,50 100,100 100,200 C100,300 300,450 300,450 C300,450 500,300 500,200 C500,100 400,50 300,150"
+            />
+            <text textAnchor="middle" x="300" y="220" className="heart-text-line">
+              Para que nunca lo pierda,
+            </text>
+            <text textAnchor="middle" x="300" y="260" className="heart-text-line">
+              se rompa, ni lo olvides.
+            </text>
+            <text textAnchor="middle" x="300" y="300" className="heart-text-line">
+              Te amo con cada parte de mí.
+            </text>
+          </svg>
+        </div>
       )}
     </div>
   );
